7802f59739699e24252bd945c0b48d5ffdbbc4ed
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2022-10-15 01:08:02 -0400

    Portal Cursor
    The portal cursor renders at the players mouse position

---
+++ Common/Systems/UISystem.cs
@@ -0,0 +1,55 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using PortalCursor.Common.UI;
+using Terraria.UI;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace PortalCursor.Common.Systems
+{
+	public class UISystem : ModSystem
+	{
+		public static UISystem Instance => ModContent.GetInstance<UISystem>();
+
+		public CursorUI CursorUI;
+		private UserInterface _interface;
+
+		public override void Load()
+		{
+			if (!Main.dedServ)
+			{
+				CursorUI = new CursorUI();
+
+				_interface = new UserInterface();
+				_interface.SetState(CursorUI);
+			}
+		}
+
+		public override void Unload()
+		{
+			CursorUI = null;
+			_interface = null;
+		}
+
+		public override void UpdateUI(GameTime gameTime)
+		{
+			_interface?.Update(gameTime);
+		}
+
+		public bool DrawGUI()
+		{
+			_interface?.Draw(Main.spriteBatch, new GameTime());
+			return true;
+		}
+
+		public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+		{
+			int inventoryLayer = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Cursor"));
+			if (inventoryLayer != -1)
+			{
+				layers.Add(new LegacyGameInterfaceLayer(
+					$"{Mod.Name}: Cursor Layer", DrawGUI, InterfaceScaleType.UI));
+			}
+		}
+	}
+}
---
+++ Common/UI/CursorUI.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using Terraria.ID;
+using ReLogic.Content;
+using Terraria.ModLoader;
+
+namespace PortalCursor.Common.UI
+{
+	public class CursorUI : UIState
+	{
+		protected override void DrawSelf(SpriteBatch spriteBatch)
+		{
+			if (Main.LocalPlayer.HeldItem.type == ItemID.PortalGun)
+			{
+				int cursorSize = 14;
+				int offset = 8;
+				Vector2 mouseCenter = new Vector2((Main.mouseX + cursorSize * Main.cursorScale / 2), Main.mouseY + cursorSize * Main.cursorScale / 2);
+
+				Asset<Texture2D> leftHalf = ModContent.Request<Texture2D>("PortalCursor/Assets/Portal_Left_Half", AssetRequestMode.ImmediateLoad);
+				Vector2 leftCenterOffset = new Vector2(11, 15);
+				Vector2 leftPos = mouseCenter -= leftCenterOffset + new Vector2(offset);
+				spriteBatch.Draw(leftHalf.Value, leftPos, leftHalf.Frame(), Color.Orange, 0, Vector2.Zero, Main.cursorScale, 0, 0);
+
+				Asset<Texture2D> rightHalf = ModContent.Request<Texture2D>("PortalCursor/Assets/Portal_Right_Half", AssetRequestMode.ImmediateLoad);
+				Vector2 rightCenterOffset = new Vector2(0, 0);
+				Vector2 rightPos = mouseCenter -= rightCenterOffset - new Vector2(offset * 2);
+				spriteBatch.Draw(rightHalf.Value, rightPos, rightHalf.Frame(), Color.SkyBlue, 0, Vector2.Zero, Main.cursorScale, 0, 0);
+			}
+		}
+	}
+}
