a991c4bbcbe931da3171585681aade170a3f84e7
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2022-10-21 13:36:23 -0400

    Configs
    Added new config settings
    
    Made mod client side

--- Common/Configs/PortalCursorConfig.cs
+++ Common/Configs/PortalCursorConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using System.ComponentModel;
+using System.Reflection.Emit;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Config;
 
@@ -15,6 +16,16 @@ namespace PortalCursor.Common.Configs
 		[Label("Use Portal Cursor")]
 		[Tooltip("Enable this to use the custom portal crosshair")]
 		public bool UsePortalCursor;
+
+		[DefaultValue(true)]
+		[Label("Use Portal Indicator")]
+		[Tooltip("Enable this to use the portal indicators")]
+		public bool UsePortalIndicator;
+
+		[DefaultValue(true)]
+		[Label("Only Enable When Using Portals")]
+		[Tooltip("With this enabled the above setting only apply when hovering over the portal station or holding the portal gun")]
+		public bool OnlyEnableWhenUsingPortals;
 
 		[DefaultValue(0)]
 		[Label("Indicator Offset")]
