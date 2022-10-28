9623795b7b25daece62b5d39317ac4875a07073a
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2022-10-28 15:06:09 -0400

    Configs and UI updates
    Added more config options
    
    Removed the UI and now do everything in draw
    
    Added a icon

--- Common/Configs/PortalCursorConfig.cs
+++ Common/Configs/PortalCursorConfig.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Terraria;
+using Microsoft.Xna.Framework;
 using System.ComponentModel;
 using System.Reflection.Emit;
 using Terraria.ModLoader;
@@ -32,8 +33,84 @@ namespace PortalCursor.Common.Configs
 		public DisplayType UsePortalIndicator;
 
 		[DefaultValue(0)]
+		[Range(-99, 99)]
 		[Label("Indicator Offset")]
 		[Tooltip("How far from the center of the cursor to draw the portal indicator")]
 		public int IndicatorOffset;
+
+		[Header("Color Settings")]
+
+		[Label("Cursor Color")]
+		[Tooltip("Changes what color to make the custom portal crosshair")]
+		[SeparatePage]
+		public ColorWrapper CursorColor = new ColorWrapper(Main.mouseColor);
+
+		[DefaultValue(true)]
+		[Label("Sync Cursor Color")]
+		[Tooltip("Enable this to make the custom portal crosshair the same color as your main cursor")]
+		public bool SyncCursorColor;
+
+		[DefaultValue(false)]
+		[Label("Invert Indicator Colors")]
+		[Tooltip("Switches the colors of the portal indicators")]
+		public bool InvertIndicatorColors;
+
+		[DefaultValue(false)]
+		[Label("Use Custom Indicator Colors")]
+		[Tooltip("Whether or not to use the colors specified in the config")]
+		public bool UseCustomIndicatorColors;
+
+		[Label("Left Indicator Color")]
+		[Tooltip("The color to make the left portal indicator")]
+		[SeparatePage]
+		public ColorWrapper LeftIndicatorColor = new ColorWrapper(Color.White);
+
+		[Label("Right Indicator Color")]
+		[Tooltip("The color to make the right portal indicator")]
+		[SeparatePage]
+		public ColorWrapper RightIndicatorColor = new ColorWrapper(Color.White);
+
+		public class ColorWrapper
+		{
+			[Label("Color")]
+			public Color Color;
+
+			public ColorWrapper(byte r, byte g, byte b, byte a)
+			{
+				Color = new Color(r, g, b, a);
+			}
+
+			public ColorWrapper(Color color)
+			{
+				this.Color = color;
+			}
+
+			public ColorWrapper()
+			{
+				Color = new Color(0, 0, 0, 0);
+			}
+
+			public override bool Equals(object obj)
+			{
+				if (obj is Color other)
+				{
+					return Color.Equals(other);
+				}
+				return base.Equals(obj);
+			}
+
+			public override int GetHashCode()
+			{
+				return Color.GetHashCode();
+			}
+		}
+
+		public override void OnChanged()
+		{
+			if (SyncCursorColor)
+			{
+				CursorColor.Color = Main.mouseColor;
+			}
+		}
 	}
 }
--- ModUtil.cs
+++ ModUtil.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using PortalCursor.Common.Configs;
 using ReLogic.Content;
+using System.Reflection;
 using Terraria;
+using Terraria.GameContent;
+using Terraria.GameInput;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -23,7 +28,75 @@ namespace PortalCursor
 
 		public static bool IsUsingPortals()
 		{
-			return Main.LocalPlayer.HeldItem.type == ItemID.PortalGun && !Main.gameMenu && !Main.InGameUI.IsVisible && !Main.ingameOptionsWindow && !Main.hideUI && !Main.mapFullscreen;
+			return (Main.LocalPlayer.HeldItem.type == ItemID.PortalGun || IsHoveringOverPortalStation()) && !Main.gameMenu && !Main.InGameUI.IsVisible && !Main.ingameOptionsWindow && !Main.hideUI && !Main.mapFullscreen && !Main.inFancyUI;
+		}
+
+		public static bool IsHoveringOverPortalStation()
+		{
+			Point pos = GetMousePos();
+			Tile tile = Framing.GetTileSafely(pos);
+
+			return tile.TileType == TileID.Cannon && tile.TileFrameX >= 216;
+		}
+
+		public static Point GetMousePos()
+		{
+			int targetX = Player.tileTargetX;
+			int targetY = Player.tileTargetY;
+			if (Main.SmartInteractShowingGenuine)
+			{
+				targetX = Main.SmartInteractX;
+				targetY = Main.SmartInteractY;
+				return new Point(targetX, targetY);
+			}
+			if (Main.SmartCursorShowing)
+			{
+				targetX = Main.SmartCursorX;
+				targetY = Main.SmartCursorY;
+				return new Point(targetX, targetY);
+			}
+			return new Point(targetX, targetY);
+		}
+
+		public static bool InBounds(int targetX, int targetY)
+		{
+			if (targetX < (Main.screenPosition.X - 16) / 16) // left
+			{
+				return false;
+			}
+			if (16 * targetX > PlayerInput.RealScreenWidth + Main.screenPosition.X) // right
+			{
+				return false;
+			}
+			if (targetY < (Main.screenPosition.Y - 16) / 16) // top
+			{
+				return false;
+			}
+			if (16 * targetY > PlayerInput.RealScreenHeight + Main.screenPosition.Y) // bottom
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public static Vector2 GetScaledCursorCenter(int cursorSize)
+		{
+			return new Vector2(Main.mouseX - cursorSize * Main.cursorScale / 2, Main.mouseY - cursorSize * Main.cursorScale / 2);
+		}
+
+		public static Vector2 GetCursorCenter(int cursorSize)
+		{
+			return new Vector2(Main.mouseX - cursorSize / 2, Main.mouseY - cursorSize / 2);
+		}
+
+		public static bool HasPlayerPlacedPortal(int portal)
+		{
+			int[,] lookup = typeof(PortalHelper).GetField("FoundPortals", BindingFlags.Static | BindingFlags.NonPublic)?.GetValue(null) as int[,];
+			if (lookup != null)
+			{
+				return lookup[Main.myPlayer, portal] != -1;
+			}
+			return false;
 		}
 
 		public static Asset<Texture2D> RequestTexture(ref Asset<Texture2D> asset, string path, AssetRequestMode mode)
